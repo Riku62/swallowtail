73da5e38ae65941284bd6913b2a70a6418522823
Riku <riku.pyon@gmail.com>
2017-05-11 11:23:34 +0900

    improved the file

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -31,12 +31,17 @@ end
 
 # No.5
 n = 10
-for i in 1..n
-  puts "#" * i
+def pyramid(n)
+  for i in 1..n
+     puts "*" * i
+  end
 end
 
 # No.6
-array = ["a" => 1, "b" => 2, "c" => 3]
+a = 1
+b = 2
+c = 3
+array = [a, b, c]
 
 # No.7
 array.each do |value|
@@ -47,21 +52,21 @@ end
 array = [1, 2, 3, 4, 5]
 num = 6
 if !(array.include?(num))
-  array.push(num)
+  array << num
 end
 p array
 
 
 # No.9
-array1 = [1,2,3,4,5]
+array1 = [1 ,2 ,3 ,4 ,5]
 def plus(array)
   sum = 0
   array.each do |num|
     sum += num
   end
-  p sum
+  sum
 end
-plus(array1)
+puts plus(array1)
 
 # No.10
 array1 = [1,2,3,4,5]
@@ -70,89 +75,102 @@ def average(array)
   array.each do |num|
     sum += num
   end
-  p sum / array.length
+  sum / array.length
 end
-average(array1)
+puts average(array1)
 
 # No.11
-def total
-
-  # No.12
-  hash = {:key1 => 1, :key2 => 2, :key => 3}
-
-  # No.13
-  hash[:key2]
-
-  # No.14
-  hash[:key4] = 4
-
-  # No.15
-  hash.each do |key, value|
-    puts "#{key} is #{value} "
-  end
-
-  # No.16
-  hash.each_value do |value|
-    value *= 2
-    puts value
-  end
-
-
-  # No.17
-  for i in 1..100
-    if i % 3 == 0 && i % 5 == 0
-      puts "FizzBuzz"
-    elsif i % 3 == 0
-      puts "Fizz"
-    elsif i % 5 == 0
-      puts "Buzz"
-    else
-      puts i
+def total(array)
+  sum = 0
+  array1 = []
+  array.each do |num|
+    sum += num
+    array1 << num
+    if sum > 15
+      break
     end
   end
+  array1
+end
 
-  # No.18
-  a1 = [1, 2, 3]
-  a2 = []
-  a1.each do |num|
-    a2.push(num * 2)
+
+# No.12
+hash = {:key1 => 1, :key2 => 2, :key => 3}
+
+# No.13
+hash[:key2]
+
+# No.14
+hash[:key4] = 4
+
+# No.15
+hash.each do |key, value|
+  puts "#{key} is #{value} "
+end
+
+# No.16
+hash.each_value do |value|
+  value *= 2
+  puts value
+end
+
+
+# No.17
+for i in 1..100
+  if i % 3 == 0 && i % 5 == 0
+    puts "FizzBuzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  elsif i % 5 == 0
+    puts "Buzz"
+  else
+    puts i
   end
+end
 
-  # No.19
-  def inc(array, num)
-    if array.include?(num)
-      return true
-    else
-      return false
+# No.18
+a1 = [1, 2, 3]
+a2 = a1.map{|n| n * 3}
+#　mapを使って記述
+
+# No.19
+def inc(array, num)
+  array.include?(num)
+end
+
+# No.20
+# jimaede
+def minimum(array)
+  number = array[0]
+  array.each do |num|
+    if number > num
+      number = num
     end
   end
+  number
+end
 
-  # No.20
-  def minimum(array)
-    array.min
-  end
+# No.21
+def duplicate?(array)
+  array.size != array.uniq.size
+end
 
-  # No.21
-  def duplication(array)
-    if array.size == array.uniq.size
-      return false
-    else
-      return true
-    end
-  end
+# No.22
+def duplicate_number_ascender(array)
+  array.select{|a| array.count(a) > 1}.uniq.sort
+end
 
-  # No.22
-  def duplicate_ascender(array)
-    array.select{|a| array.count(a) > 1}.uniq.sort
+# No.23
+def duplicate_number_hash(array)
+  array1 = array.select{|a| array.count(a) > 1}.sort
+  p array1
+  k = Hash.new(0)
+  array1.each do |num|
+    k[num] = array1.count(num)
   end
-
-  # No.23
-  def duplicate_hash(array)
-    array1 = array.select{|a| array.count(a) > 2}.sort
-    p array1
-    k = Hash.new(0)
-    array1.each do |num|
-      k[num] = array1.count(num)
-    end
-    puts k
-  end
+  k 
+end
+# 修正版
+def duplicate_number_ascender(array)
+  array.reject { |a| array.index(a) == array.rindex(a) }.each_with_object(Hash.new(0)) { |a, h| h[a] += 1;}
+end
